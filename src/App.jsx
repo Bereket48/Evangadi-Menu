ae843ddbaf6c73bbd37de4f36c1f029afe41efaf
Bereket48 <abatebereket536@gmail.com>
2025-04-18 04:06:05 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import FoodItem from './Components/FoodItem/FoodItem'
+
+function App() {
+  return (
+    <>
+      FoodItem
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/FoodItem/FoodItem.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+ class FoodItem extends Component {
+  render() {
+    return (
+      <>FoodItem</>
+    )
+  }
+}
+
+export default FoodItem;
