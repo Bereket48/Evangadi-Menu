f4d1d443fa92d74914dff2477b2a63a16c190f83
Bereket48 <abatebereket536@gmail.com>
2025-04-20 02:14:58 +0200

    Menu built

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import "../src/assets/style.css"
-import Header from './Components/Header/Header'
-import AllFoodItems from './Components/AllFoodItems/AllFoodItems'
+import "../src/assets/style.css";
+import Header from "./Components/Header/Header";
+import AllFoodItems from "./Components/AllFoodItems/AllFoodItems";
 function App() {
   return (
     <>
-      <div>Header</div>
-      <div>AllFoodItems</div>
+      <div className="all-container">
+        <Header />
+        <AllFoodItems />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/AllFoodItems/AllFoodItems.jsx
+++ src/Components/AllFoodItems/AllFoodItems.jsx
@@ -1,13 +1,26 @@
-import { Component } from 'react'
-import FoodItem from './FoodItem/FoodItem';
-
+import { Component } from "react";
+import FoodItem from "./FoodItem/FoodItem";
+import { menu } from "../../assets/data";
 class AllFoodItems extends Component {
   render() {
     return (
-      <>FoodItem</>
-    )
+      <>
+        <div className="foods-container">
+          {menu.map(function (element, index) {
+            return (
+              <FoodItem
+                img={element.img}
+                desc={element.desc}
+                key={element.id}
+                title={element.title}
+                price={element.price}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
   }
 }
-
 
 export default AllFoodItems;
